596c06f5b0827f48180c9995dfa06b7b4801e3a1
Jacob Feltman <jacob.feltman@gmail.com>
2021-07-22 11:58:56 -0600

    setup script

--- preNewSystemSetupFish.fish
+++ preNewSystemSetupFish.fish
@@ -1,4 +1,5 @@
 # Fish shell should already be installed at this point
+sudo apt -y install curl
 chsh -s /usr/bin/fish
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > install
 fish install
