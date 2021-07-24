1f09cb387d9e2e910b08d0f0b5034807eef8112e
Jacob Feltman <jacob.feltman@gmail.com>
2021-07-23 21:53:41 -0600

    pacman apt adjustment

--- preNewSystemSetupFish.fish
+++ preNewSystemSetupFish.fish
@@ -1,5 +1,6 @@
 # Fish shell should already be installed at this point
-sudo pacman -S --noconfirm curl
+# sudo pacman -S --noconfirm curl # Arch-based
+sudo apt -y install curl # Debian-based
 chsh -s /usr/bin/fish
 curl -L https://github.com/oh-my-fish/oh-my-fish/raw/master/bin/install > install
 fish install
